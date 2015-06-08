3c13badf6fa16cfe8478cebc03bf2597796c0419
Michael_qin <27206232@qq.com>
2015-06-08 17:39:44 +0800

    add layout main

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 	}
 }
